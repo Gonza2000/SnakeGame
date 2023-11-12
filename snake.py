4b900b5d4f32ca6fa6cae80199bf95fec530b0a9
Gonzalo Cardenas <gonza.card2000@hotmail.com>
2023-11-11 21:12:43 -0500

    El cuerpo de la serpiente toma el color de la manzana que comió

--- snake.py
+++ snake.py
@@ -77,7 +77,8 @@ pantalla.onkeypress(arriba, 'W')
 pantalla.onkeypress(abajo, 'S')
 
 # Elemento Manzana
-manzana = crear_elemento('circle', '#FF004D', 0)
+color_random = ["#" + "".join([random.choice("0123456789ABCDEF") for j in range(6)])]
+manzana = crear_elemento('circle', color_random, 0)
 manzana.setposition(200, 100)
 
 
@@ -97,23 +98,27 @@ while True:
 
     # Cambio de posición de la Manzana
     if cabeza.distance(manzana) < 50:
+        color_manzana = manzana.pencolor()
         x = random.randint(-620, 620)
         y = random.randint(-340, 340)
         manzana.goto(x, y)
-        cuerpo = crear_elemento('square', '#FF004D', 0)
+        color_random = ["#" + "".join([random.choice("0123456789ABCDEF") for j in range(6)])]
+        manzana.color(color_random)
+        cuerpo = crear_elemento('square', color_manzana, 0)
         cuerpo_snake.append(cuerpo)
 
    
+    if len(cuerpo_snake) > 0:
+        x = cabeza.xcor()
+        y = cabeza.ycor()
+        cuerpo_snake[0].goto(x, y)    
         
     for i in range(len(cuerpo_snake) - 1, 0, -1):
         x = cuerpo_snake[i - 1].xcor()
         y = cuerpo_snake[i - 1].ycor()
         cuerpo_snake[i].goto(x, y)
 
-    if len(cuerpo_snake) > 0:
-        x = cabeza.xcor()
-        y = cabeza.ycor()
-        cuerpo_snake[0].goto(x, y)
+    
 
     movimiento_snake()
     time.sleep(delay)
